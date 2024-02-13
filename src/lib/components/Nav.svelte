e469d1796f72ed6668b43529514d1d86ce6e9d4e
Anton Jönsson <anton.jonsson@gmail.com>
2024-02-13 01:35:53 +0100

    Nav icon sizing fixed

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Nav from './Nav.svelte';
 	import URL from './URL.js'
-	import { authenticated } from '../stores/auth';
+	import { authenticated } from '$lib/stores/auth';
 	import { page } from '../stores/page';
 	// import {page} from '$app/stores';
 	import { goto } from '$app/navigation';
@@ -94,18 +94,32 @@
 					<path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/>
 				</svg>
 				<span class="link-text">Home</span>
-			</a></li>
+			</a>
+		</li>
+		{#if $authenticated}
 		<li class="nav-item" class:selected={$page=="login"}>
-			<a href="/login" class="nav-link">
+			<a href="/" class="nav-link">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
 				</svg>
 				<span class="link-text">Login</span>
-			</a></li>
-		<li class="nav-item" class:selected={$page=="register"}>
-			<a href="/register" class="nav-link">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M192 0c-41.8 0-77.4 26.7-90.5 64H64C28.7 64 0 92.7 0 128V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H282.5C269.4 26.7 233.8 0 192 0zm0 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM112 192H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg>
-				<span class="link-text">Register</span>
-			</a></li>
+			</a>
+		</li>
+		{:else}
+			<li class="nav-item" class:selected={$page=="login"}>
+				<a href="/login" class="nav-link">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
+					</svg>
+					<span class="link-text">Login</span>
+				</a>
+			</li>
+			<li class="nav-item" class:selected={$page=="register"}>
+				<a href="/register" class="nav-link">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M192 0c-41.8 0-77.4 26.7-90.5 64H64C28.7 64 0 92.7 0 128V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H282.5C269.4 26.7 233.8 0 192 0zm0 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM112 192H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg>
+					<span class="link-text">Register</span>
+				</a>
+			</li>
+		{/if}
+		
 	</ul>
 </nav>
 
@@ -164,10 +178,7 @@
 		margin-left: 1rem;
 	}
 
-	.nav-link svg{
-		min-width: 2rem;
-		margin: 0 1.5rem;
-	}
+
 
 
 
@@ -186,7 +197,12 @@
 	.navbar:hover .link-text{
 		display: inline;
 	}
-
+	
+	.nav-link svg{
+		max-width: 2rem;
+		margin: 0 1.5rem;
+	}
+	
 }
 
 @media only screen and (max-width: 600px) {
@@ -207,6 +223,11 @@
 
 	.nav-link{
 		justify-content: center;
+	}
+	
+	.nav-link svg{
+		height:max(2rem);
+		margin: 0 1.5rem;
 	}
 }
 
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,6 +3,8 @@
 	import LoginWorker from '../../lib/components/Login/LoginWorker.svelte';
 	import { page } from '$lib/stores/page';
 	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import URL from "$lib/components/URL.js"
 
 	onMount(()=> $page = "login")
 
@@ -10,6 +12,32 @@
 
 	const setClient = () => (loginSwitch = 'CLIENT');
 	const setWorker = () => (loginSwitch = 'WORKER');
+
+		
+	let email = '',
+		password = '';
+		
+	const submitClient = async () => {
+		console.log('Submit:', 'Email:', email, 'Password:', password);
+		await fetch(URL(loginSwitch == "CLIENT"?'login/client':'login/worker'), {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+				credentials: 'include',
+				body: JSON.stringify({
+				email,
+				password,
+				'role':loginSwitch
+			})
+		})
+		.then((res) => console.log(res))
+		.catch((err) => console.log(err));
+		
+		goto('/');
+	};
+
+
+
+
 </script>
 <div class="logo_container">
 	<h1>Sign in</h1>
@@ -22,12 +50,31 @@
 	</div>
 	<div class="container">
 		<br>
+
 		
-		{#if loginSwitch == 'CLIENT'}
+		
+		<form on:submit|preventDefault={submitClient}>
+			<div class="form_item">
+				<h4>Email:</h4>
+				<input bind:value={email} type="email" name="email" placeholder="Email" />
+			</div>
+			<br />
+			{#if loginSwitch=='CLIENT'}
+				<div class="form_item ">
+					<h4>Password:</h4>
+					<input bind:value={password} type="password" name="password" placeholder="Password" />
+				</div>
+				<br />
+			{/if}
+		
+			<button type="submit"> Login </button>
+		</form>
+		
+		<!-- {#if loginSwitch == 'CLIENT'}
 			<LoginClient />
 		{:else}
 			<LoginWorker />
-		{/if}
+		{/if} -->
 	</div>
 </div>
 
