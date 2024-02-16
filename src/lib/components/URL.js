e0caafb2ab53996a7ba973dca35890131ba13380
Anton Jönsson <anton.jonsson@gmail.com>
2024-02-16 21:20:20 +0100

    buildings page added

--- src/lib/components/URL.js
+++ src/lib/components/URL.js
@@ -1,3 +1,4 @@
 export default  function (props){
-        return "https://0b786f99-e108-444b-a1f8-bfe60f94e161-00-398ktbyqmwccj.picard.repl.co/" + props
+        return "http://localhost:3000/" + props;
+        // return "https://0b786f99-e108-444b-a1f8-bfe60f94e161-00-398ktbyqmwccj.picard.repl.co/" + props
     }
